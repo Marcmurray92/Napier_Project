22511f5887417fc1d20668a34463a8571d839fbf
Marcmurray92 <Marc.Murray.92@gmail.com>
2015-02-12 14:35:50 +0000

    Lightboxes to go.
    More defined layout, filtering complete, nav almost done. After this,
    just the lightboxes to go.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
-
-
- // Menu Toggle Script
- 
-
-  $("#menu-toggle").click(function(e) {
+ // Menu Toggle Script //
+$("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
   });
 
+ // Menu Toggle Script //
+$("#nav-menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
 
-
-// Hail Delete Script
-  $("#closer").click(function() {
-    $("#hail").animate({
+// Hail Delete, Activate Nav Script //
+$("#closer").click(function() {
+$("#topcon").animate({
 opacity: 0.15,
-left: "+=500",
 height: "toggle"
 }, 300, function() {
 // Animation complete.
 });
   });
+$("#closer").click(function() {
+$("#nav").show({}, 300, function() {
+// Animation complete.
+});
+  });
+
+// Initiate Mixitup //
+
+$(function(){
+  $('#Container').mixItUp({
+    load: {
+      filter: '' 
+    },
+    controls: {
+      toggleFilterButtons: true,
+      toggleLogic: 'and'
+    },
+    callbacks: {
+      onMixEnd: function(state){
+        console.log(state.activeFilter)
+      }
+    }
+  });
+});
